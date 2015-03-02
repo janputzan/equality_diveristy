c537b741a3307f631c1a309ec546b3fb44ec2a36
janputzan <janputzan@hotmail.co.uk>
2015-03-02 16:40:21 +0000

    created test_questions, questions, answers and categories tables

---
+++ app/database/migrations/2015_03_02_162140_create_questions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('questions', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('right_answer_id')->unsigned();
+			$table->integer('category_id')->unsigned();
+			$table->text('body');
+
+			$table->index('right_answer_id');
+			$table->index('category_id');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('questions');
+	}
+
+}
