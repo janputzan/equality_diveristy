a8021444a616bf75c0aeff9c63cf7a59ecda119c
janputzan <janputzan@hotmail.co.uk>
2015-03-02 16:06:55 +0000

    created users table

--- app/database/migrations/2015_03_02_152550_create_users_table.php
+++ app/database/migrations/2015_03_02_152550_create_users_table.php
@@ -16,7 +16,15 @@ class CreateUsersTable extends Migration {
 		{
 		    $table->increments('id');
 		    $table->string('email');
-		    $table->
+		    $table->string('password');
+		    $table->string('first_name');
+		    $table->string('last_name');
+		    $table->integer('manager_id')->unsigned()->nullable();
+
+		    $table->timestamps();
+
+		    $table->index('email');
+		    $table->index('manager_id');
 		});
 	}
 
@@ -27,7 +35,7 @@ class CreateUsersTable extends Migration {
 	 */
 	public function down()
 	{
-		//
+		Schema::drop('users');
 	}
 
 }
